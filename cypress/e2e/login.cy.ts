20665d5a86b96561d6d9f6c3327085cc87a24af5
hiepnguyen6014 <hiepnguyen6014@gmail.com>
2023-07-11 23:42:08 +0700

    feat: a

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,6 +1,6 @@
 describe("check login page", () => {
-  it("get username label", () => {
+  it("check input have type password", () => {
     cy.visit("http://localhost:8080/login");
-    cy.get("label").should("have.text", "Username");
-  }
+    cy.get("#basic_password").should("have.attr", "type", "password");
+  });
 });
